d7270c53669c099774b344b23fb610f318630d8d
Stanislav Nepochatov <spoilt@blackstarfilm.com>
2020-04-21 01:44:56 +0300

    Refactor structure.

--- directory-unit/src/main/java/tk/freaxsoftware/ribbon2/directory/UnitMain.java
+++ directory-unit/src/main/java/tk/freaxsoftware/ribbon2/directory/UnitMain.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tk.freaxsoftware.ribbon2.authprovider;
+package tk.freaxsoftware.ribbon2.directory;
 
 import tk.freaxsoftware.extras.bus.MessageBus;
 import tk.freaxsoftware.extras.bus.MessageHolder;
--- message-unit/src/main/java/tk/freaxsoftware/ribbon2/message/UnitMain.java
+++ message-unit/src/main/java/tk/freaxsoftware/ribbon2/message/UnitMain.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.
  */
-package tk.freaxsoftware.ribbon2.test.unit;
+package tk.freaxsoftware.ribbon2.message;
 
 import java.util.UUID;
 import tk.freaxsoftware.extras.bus.Callback;
