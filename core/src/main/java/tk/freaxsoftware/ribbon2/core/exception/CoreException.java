a0b7fd253b3cee132fe0057e28744cf0cd2e4221
Stanislav Nepochatov <spoilt@blackstarfilm.com>
2020-04-24 18:31:52 +0300

    Add core exception;

---
+++ core/src/main/java/tk/freaxsoftware/ribbon2/core/exception/CoreException.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of Ribbon2 news message system.
+ * 
+ * Copyright (C) 2020 Freax Software
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+package tk.freaxsoftware.ribbon2.core.exception;
+
+/**
+ * Core exception with provided code.
+ * @author Stanislav Nepochatov
+ */
+public class CoreException extends RuntimeException {
+    
+    private final String code;
+
+    public CoreException(String code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public CoreException(String code, String message, Throwable cause) {
+        super(message, cause);
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return "[" + this.code + "]: " + super.getMessage();
+    }
+    
+    
+}
