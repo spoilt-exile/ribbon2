400c8272fae6ef01a5634e43dd80174a9e2f8868
Stanislav Nepochatov <spoilt.exile@gmail.com>
2022-01-04 15:09:07 +0200

    #24 Fix API according to docs;

--- core/src/main/java/tk/freaxsoftware/ribbon2/core/data/response/DefaultPage.java
+++ core/src/main/java/tk/freaxsoftware/ribbon2/core/data/response/DefaultPage.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of Ribbon2 news message system.
  * 
- * Copyright (C) 2020 Freax Software
+ * Copyright (C) 2022 Freax Software
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,31 +16,36 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.
  */
-package tk.freaxsoftware.ribbon2.gateway.entity.converters;
+package tk.freaxsoftware.ribbon2.core.data.response;
 
+import io.ebean.PagedList;
+import java.util.List;
 import java.util.stream.Collectors;
-import tk.freaxsoftware.ribbon2.core.data.UserModel;
 import tk.freaxsoftware.ribbon2.core.data.convert.Converter;
-import tk.freaxsoftware.ribbon2.gateway.entity.UserEntity;
 
 /**
- * Converter for users;
+ * Default page implementation with built-in conversion of entity.
  * @author Stanislav Nepochatov
  */
-public class UserConverter implements Converter<UserEntity, UserModel>{
-
-    @Override
-    public UserModel convert(UserEntity source) {
-        UserModel user = new UserModel();
-        user.setId(source.getId());
-        user.setLogin(source.getLogin());
-        user.setFirstName(source.getFirstname());
-        user.setLastName(source.getLastname());
-        user.setEnabled(source.getEnabled());
-        user.setEmail(source.getEmail());
-        user.setDescription(source.getDescription());
-        user.setGroups(source.getGroups().stream().map(gr -> gr.getName()).collect(Collectors.toSet()));
-        return user;
+public class DefaultPage<T, C> implements Page<C> {
+    
+    private List<C> content;
+    
+    private long totalCount;
+    
+    public DefaultPage(PagedList<T> pagedList, Converter<T, C> converter) {
+        content = pagedList.getList().stream().map(r -> converter.convert(r)).collect(Collectors.toList());
+        totalCount = pagedList.getTotalCount();
     }
 
+    @Override
+    public List<C> getContent() {
+        return content;
+    }
+
+    @Override
+    public long getTotalCount() {
+        return totalCount;
+    }
+    
 }
