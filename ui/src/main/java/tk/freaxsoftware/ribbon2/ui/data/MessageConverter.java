a25b8d21551a70c8e844960adc406fc94d45e9b3
Stanislav Nepochatov <spoilt.exile@gmail.com>
2022-05-10 17:24:28 +0300

    #22 Add lazy loading pagination for messages;

---
+++ ui/src/main/java/tk/freaxsoftware/ribbon2/ui/data/MessageConverter.java
@@ -0,0 +1,42 @@
+/*
+ * This file is part of Ribbon2 news message system.
+ * 
+ * Copyright (C) 2020-2022 Freax Software
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+package tk.freaxsoftware.ribbon2.ui.data;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import tk.freaxsoftware.ribbon2.core.data.MessageModel;
+
+/**
+ *
+ * @author spoilt
+ */
+public class MessageConverter implements Converter<MessageModel> {
+
+    @Override
+    public MessageModel getAsObject(FacesContext fc, UIComponent uic, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getAsString(FacesContext fc, UIComponent uic, MessageModel t) {
+        return t.getId().toString();
+    }
+    
+}
